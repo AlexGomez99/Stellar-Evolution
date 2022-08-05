faa0c2b7e5689c4caeb93d330bd720e5110302ca
ekfin <efindlay@ucsd.edu>
2022-08-05 16:49:29 -0700

    added audio :)

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,6 +6,41 @@ public class AudioManager : MonoBehaviour
 {
 
     public AudioSource matchSound;
+    public AudioSource bloop2;
+    public AudioSource bloop1;
+    public AudioSource bloop3;
+    public AudioSource bloop4;
+    public AudioSource bloop5;
+    public AudioSource bloop6;
+    public AudioSource anotherAscension;
+    public AudioSource ascendingSaucer;
+    public AudioSource bigSqueep;
+    public AudioSource bloopAscended;
+    public AudioSource brilliant2;
+    public AudioSource buzzer;
+    public AudioSource coinSound;
+    public AudioSource cosmic3;
+    public AudioSource error3;
+    public AudioSource highScore1;
+    public AudioSource highScore4;
+    public AudioSource illegalMove2;
+    public AudioSource interstellar3;
+    public AudioSource luminous3;
+    public AudioSource magneticAnomaly1;
+    public AudioSource outOfThisWorld1;
+    public AudioSource powerDown;
+    public AudioSource radiant1;
+    public AudioSource solarFlare3;
+    public AudioSource spectacular1;
+    public AudioSource spectacular4;
+    public AudioSource squeep;
+    public AudioSource stellar1;
+    public AudioSource stellarMatch2; 
+    public AudioSource transcendentBloop;
+    public AudioSource tryAgain3;
+    public AudioSource tryAgain4;
+    public AudioSource winner2;
+
     // Start is called before the first frame update
     void Start()
     {
